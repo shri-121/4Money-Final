7de82ea54273b6ba034d0e44e9d2bd0663f50250
Shreyas Chauhan <shrichauhan121@gmail.com>
2026-04-24 05:05:00 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,3 +1,4 @@
+print("🚀 Server file started loading...")
 from fastapi import FastAPI, APIRouter, HTTPException, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi.middleware.cors import CORSMiddleware
